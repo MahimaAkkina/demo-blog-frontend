ea521bb61234a14a1e2a85d2949e2c687cfec26f
MahimaAkkina <mahimaakkina@gmail.com>
2026-02-17 14:22:44 +0530

    Added a comment

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,20 @@
-import Link from "next/link";
+import Link from "next/link"; // for not reloading the page, for fast navigation
 
-export default async function Home() {
+export default async function Home() { // timetaken for fetching data from strapi so async
   const response = await fetch(
-    "https://tidy-attraction-06e886b553.strapiapp.com/api/articles?populate=*"
+    "https://tidy-attraction-06e886b553.strapiapp.com/api/articles?populate=*" // populate=* it gives all fields
   );
-  const result = await response.json();
+  const result = await response.json(); // turning raw data in readable json format
   const blogs = result.data;
-  console.log(blogs)
+  console.log(blogs) // loop
 
   return (
     <div>
       <h1 className="text-3xl font-bold">All Blogs</h1>
 
-      {blogs.map((blog) => (
+      {blogs.map((blog) => ( // arrow func - map method checks each and every blog in array of blogs
         <div key={blog.id} className="my-4">
-          <Link href={`/blogs/${blog.slug}`}>
+          <Link href={`/blogs/${blog.slug}`}> 
             <h2 className="text-xl text-blue-500 cursor-pointer">
               {blog.title}
             </h2>
